6963c5b3779351bdf5a778638f04f4da6f367884
Mario Bandeira <josemariobandeira@hotmail.com>
2024-02-25 22:35:46 -0300

    feat(component): add generic formulary component

---
+++ src/components/molecules/MFormulary.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-form v-bind="$attrs" @submit.prevent="$emit('submit', $event)">
+    <slot></slot>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+      <v-btn type="submit">Submit</v-btn>
+    </div>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "MFormulary",
+}
+</script>
+
+<style lang="">
+  
+</style>
