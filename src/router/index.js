2a442bc2d60131faf7c5e4607e6639d91c4e304d
Mario Bandeira <josemariobandeira@hotmail.com>
2024-02-26 08:01:18 -0300

    feat: add router navigation guard rules

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,49 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../components/views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../components/views/HomeView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "LOGIN",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "ABOUT",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../components/views/AboutView.vue"
+      ),
+  },
+];
+
+VueRouter.prototype.$token = localStorage.getItem('token') || ''
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+const publicRoutes = ["LOGIN"];
+
+router.beforeEach((to, from, next) => {
+  const isPublicRoute = publicRoutes.includes(to.name);
+
+  if (router.$token.length) {
+    if (isPublicRoute) return next({ name: 'ABOUT' })
+    else next()
+  } else {
+    if (!isPublicRoute) return next({ name: 'LOGIN' })
+  }
+
+  return next()
+});
+
+export default router;
